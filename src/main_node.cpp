8b8a3dc0bb0ab8e3767c38e6cf7b514090bda9ba
Jonathan West <jmwest@unm.edu>
2016-12-19 18:56:06 +0000

    added serial port

--- src/main_node.cpp
+++ src/main_node.cpp
@@ -3,12 +3,16 @@
 #include <fstream>
 #include "ros/ros.h"
 #include "sensor_msgs/Image.h"
+#include "serial/serial.h"
 
 using namespace std;
+serial::Serial serial_port("/dev/ttyAMA0", 9600, 
+	serial::Timeout::simpleTimeout(1000));
 
 void cameraCallback(const sensor_msgs::Image::ConstPtr& msg)
 {
 	ROS_INFO("Got an image");
+	serial_port.write("Got an image");
 }
 
 int main(int argc, char** argv)
@@ -17,6 +21,13 @@ int main(int argc, char** argv)
 	ros::NodeHandle n;
 	ros::Subscriber camera_sub = n.subscribe("raspicam",1000, 
 		cameraCallback);
+	if(serial_port.isOpen()) {
+		cout << "Serial Port opened\n";
+	} else {
+		cout << "Serial Port FAILED\n";
+	}
+
+	
 	ros::spin();	
 	return 0;
 }
