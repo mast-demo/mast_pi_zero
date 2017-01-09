1a7005d6a8fd0e24492789dfc5af23589c3eb68f
Jonathan West <jmwest@unm.edu>
2017-01-09 16:22:22 +0000

    added check for empty ROS_IP in run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
+#give network time to DHCP.  otherwise ROS_IP is empty and ROS will not work on network
+sleep 5s
 export ROS_IP=`hostname -I`
-roslaunch mast_pi_zero mast_pi_zero.launch name:=`hostname`
+if [[ -z $ROS_IP ]] ; then
+	echo "ROS_IP is empty, no network?"
+	echo "Sleeping and trying again"
+	sleep 10s
+	export ROS_IP=`hostname -I`
+	echo "ROS_IP = $ROS_IP"
+else
+	echo "ROS_IP = $ROS_IP"
+fi
+if [[! -z $ROS_IP ]] ; then
+	source ~/ros_catkin_ws/devel/setup.bash
+	roslaunch mast_pi_zero mast_pi_zero.launch name:=`hostname`
+fi
